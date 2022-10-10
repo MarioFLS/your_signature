4175cdbb5b4144f54a4ae802568268ec0e631f64
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-09 22:18:47 -0300

    feat: Pegar imagens pelo localhost

--- product/app.js
+++ product/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('express-async-errors');
 const cors = require('cors');
 require('dotenv').config();
@@ -6,6 +7,11 @@ require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 4200;
+
+const uploadDirectory = path.join(__dirname, 'public');
+
+app.use('/images', express.static(uploadDirectory));
+
 app.get('/', (_req, res) => res.status(200)
   .json({ bem_vindo: 'Seja Bem vindo a aplicação: Product' }));
 

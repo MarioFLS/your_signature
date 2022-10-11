33fd81a7564f7b1d89953bd96ef361cf90f6bf18
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-10 23:23:05 -0300

    feat: Middleware de captura de erro

---
+++ userProducts/src/middleware/middleware.error.js
@@ -0,0 +1,12 @@
+const Error = (err, _req, res, _next) => {
+  const { error } = err;
+  if (error) {
+    return res.status(error.code).json({ message: error.message });
+  }
+  return res.status(500).json({
+    message:
+        'Tivemos um erro Interno, aguarde um tempo e recarregue a página!',
+  });
+};
+
+module.exports = { Error };

2420f571140993677647743af33274ce1543b640
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-11 09:39:44 -0300

    feat: Adição da rota de criação de assinatura

--- userProducts/app.js
+++ userProducts/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { newSignature } = require('./src/controllers/controller.signature');
 const { database, closeDatabase } = require('./src/models/connection');
 const { Signatures } = require('./src/models/signatures');
 require('express-async-errors');
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 4300;
 
 app.use(express.json());
 Signatures();
+
+app.post('/new', newSignature);
 
 app.get('/', (_req, res) => res.status(200).json({
   bem_vindo: 'Seja Bem vindo a aplicação: User Product',

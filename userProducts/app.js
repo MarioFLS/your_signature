1acc2f0faf438e7b040e433444cb4cb186c4922e
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-10 22:43:01 -0300

    feat: Chamada da tabela

--- userProducts/app.js
+++ userProducts/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { database, closeDatabase } = require('./src/models/connection');
+const { Signatures } = require('./src/models/signatures');
 require('express-async-errors');
 require('dotenv').config();
 
@@ -7,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 4300;
 
 app.use(express.json());
+Signatures();
 
-app.get('/', (_req, res) =>
-  res.status(200).json({
-    bem_vindo: 'Seja Bem vindo a aplicação: User Product',
-  })
-);
+app.get('/', (_req, res) => res.status(200).json({
+  bem_vindo: 'Seja Bem vindo a aplicação: User Product',
+}));
 
-app.listen(PORT, console.log('Rodando na porta ', PORT));
+app.listen(PORT, () => console.log(`Rodando na porta: ${PORT}`));
+closeDatabase();

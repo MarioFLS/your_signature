b30d7f5f323f58d3d3bc8c6438df0e59c6572931
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-11 14:41:52 -0300

    feat: adição na api central

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const PORT = 4000;
 app.use(
   morgan(':method - :url - status: :status - response time: :response-time ms ')
 );
+
 app.use('/user', proxy('http://localhost:3100'));
 app.use('/product', proxy('http://localhost:3200'));
+app.use('/signatures', proxy('http://localhost:3300'));
 
 app.get('/', (_req, res) =>
   res.status(200).json({ bem_vindo: 'Seja Bem vindo a aplicação' })

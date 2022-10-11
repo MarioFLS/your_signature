42c345f1a81a5f083a1f0e2ae8a698a92f0594b4
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-11 11:22:43 -0300

    refactor: Correção na validação

--- userProducts/src/middleware/validate.create.signature.js
+++ userProducts/src/middleware/validate.create.signature.js
@@ -5,52 +5,46 @@ const requiredItemString = joi.string().not().empty().required();
 const requiredItemNumber = joi.number().min(1).required();
 
 const validateSignature = (req, res, next) => {
-  const {
-    userId, quantity, productId, productName, productImage, signedIn,
-
-  } = req.body;
-  const loginValidation = joi.object({
-    userId: requiredItemNumber,
+  const loginValidation = joi.array().items(joi.object({
     quantity: requiredItemNumber,
     productId: requiredItemNumber,
     productName: requiredItemString,
     productImage: requiredItemString,
     signedIn: joi.date().not().empty().required(),
-  })
-    .messages({
-      'any.required': 'Insira os dados corretamentos',
-      'string.empty': 'Insira o nome do produto',
-      'number.min': 'Insira um Id de usuário válido',
-    });
-
-  const { error } = loginValidation.validate({
-    userId, quantity, productId, productName, productImage, signedIn,
+  })).messages({
+    'any.required': 'Insira os dados corretamentos',
+    'string.empty': 'Insira o nome do produto',
+    'number.min': 'Insira um Id de produto válido',
   });
+  const { error } = loginValidation.validate(req.body.map((item) => item));
   const type = error?.details[0].context?.key;
+  const index = error?.details[0].path[0];
   if (error) {
-    if (type === 'productId') {
-      return res.status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'Insira um Id de produto válido' });
-    }
     if (type === 'productName') {
       return res.status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'O nome do produto não pode estar vázio' });
+        .json({
+          message: 'O nome do produto não pode estar vázio',
+          index,
+        });
     }
     if (type === 'productImage') {
       return res.status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'A imagem do produto não pode estar vázia' });
+        .json({
+          message: 'A imagem do produto não pode estar vázia',
+          index,
+        });
     }
     if (type === 'quantity') {
       return res.status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'Insira uma quantidade acima de 0' });
+        .json({ message: 'Insira uma quantidade acima de 0', index });
     }
     if (type === 'signedIn') {
       return res.status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'Insira uma data válida' });
+        .json({ message: 'Insira uma data válida', index });
     }
 
     return res.status(StatusCodes.UNAUTHORIZED)
-      .json({ message: error.details[0].message });
+      .json({ message: error.details[0].message, index });
   }
   return next();
 };

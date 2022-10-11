6b638a70157337857d0a72e1f9cd26131ea88220
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-11 11:24:15 -0300

    refactor: rota de criar nova assinatura

--- userProducts/app.js
+++ userProducts/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { newSignature } = require('./src/controllers/controller.signature');
+const { tokenValidate } = require('./src/middleware/validate.token');
+const { Error } = require('./src/middleware/middleware.error');
 const { validateSignature } = require('./src/middleware/validate.create.signature');
 const { closeDatabase } = require('./src/models/connection');
 const { Signatures } = require('./src/models/signatures');
@@ -13,11 +15,12 @@ const PORT = process.env.PORT || 4300;
 app.use(express.json());
 Signatures();
 
-app.post('/new', validateSignature, newSignature);
+app.post('/new', validateSignature, tokenValidate, newSignature);
 
 app.get('/', (_req, res) => res.status(200).json({
   bem_vindo: 'Seja Bem vindo a aplicação: User Product',
 }));
 
+app.use(Error);
 app.listen(PORT, () => console.log(`Rodando na porta: ${PORT}`));
 closeDatabase();

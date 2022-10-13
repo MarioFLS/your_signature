7491146def6f7a6fbe53a4fea1eed53d20427fa5
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-12 21:20:58 -0300

    refactor: Buscar assinaturas pelo ID do usuário

--- userProducts/src/controllers/controller.signature.js
+++ userProducts/src/controllers/controller.signature.js
@@ -8,11 +8,11 @@ const newSignature = async (token, req, res, _next) => {
   await Promise.all(signatures
     .map((signature) => createNewSignature({ userId: token.id, ...signature })));
 
-  return res.status(200).json({ message: 'Sucesso' });
+  return res.status(201).json({ message: 'Sucesso' });
 };
 
-const getSignatures = async (req, res) => {
-  const response = await getSignatureByUserID(req.params.id);
+const getSignatures = async (token, _req, res, _next) => {
+  const response = await getSignatureByUserID(token.id);
   return res.status(200).json(response);
 };
 

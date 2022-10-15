a08642f60372c8610c40f8780b5101920d6791c3
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-14 22:08:45 -0300

    feat: script create

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(
   morgan(':method - :url - status: :status - response time: :response-time ms ')
